a298b7586e448f2a7c431ed30b7be2ed7cbd8478
SUJAL-DESAI <sujaldesai4903@gmail.com>
2025-03-15 22:01:03 +0530

    updated name

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ const App = () => {
 
   const handledownload = () => {
     const link = document.createElement('a');
-    link.href = '/SUJAL (4).pdf'; // Path to the PDF file in the public folder
-    link.download = '/SUJAL (4).pdf'; // Desired file name when downloaded
+    link.href = '/SUJAL.pdf'; // Path to the PDF file in the public folder
+    link.download = '/SUJAL.pdf'; // Desired file name when downloaded
     link.click();
   }
 
